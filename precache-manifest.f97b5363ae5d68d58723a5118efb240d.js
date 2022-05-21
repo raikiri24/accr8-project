2d263a3255115a4f932e53104313654ea0992959
Paul Leandro Lanot <kraft091@gmail.com>
2022-05-21 21:15:46 +0800

    Updates

---
+++ precache-manifest.f97b5363ae5d68d58723a5118efb240d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d43000ca85c2d09fa2e",
+    "url": "/static/css/main.b0f7ac9e.chunk.css"
+  },
+  {
+    "revision": "2d43000ca85c2d09fa2e",
+    "url": "/static/js/main.2d43000c.chunk.js"
+  },
+  {
+    "revision": "19f43b1ef2893daf19a8",
+    "url": "/static/js/1.19f43b1e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75cdab355b11aaa1c4f6fdd8e652b128",
+    "url": "/index.html"
+  }
+];
