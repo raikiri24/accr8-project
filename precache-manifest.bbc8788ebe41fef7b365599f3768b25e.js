932fa82e6560cc09e9fa0e9148d7d1f34296263e
Paul Leandro Lanot <kraft091@gmail.com>
2022-05-21 21:37:33 +0800

    Updates

---
+++ precache-manifest.bbc8788ebe41fef7b365599f3768b25e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8c7209a224264fed388",
+    "url": "/accr8-project/static/css/main.b0f7ac9e.chunk.css"
+  },
+  {
+    "revision": "c8c7209a224264fed388",
+    "url": "/accr8-project/static/js/main.c8c7209a.chunk.js"
+  },
+  {
+    "revision": "19f43b1ef2893daf19a8",
+    "url": "/accr8-project/static/js/1.19f43b1e.chunk.js"
+  },
+  {
+    "revision": "a4034ce600b5a91103ae",
+    "url": "/accr8-project/static/js/runtime~main.a4034ce6.js"
+  },
+  {
+    "revision": "8e2a9d67bf29c8e4cb2f2ed61fc58a3e",
+    "url": "/accr8-project/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26037303d77cf3d02b2ceac499cd70f4.js"
+  "/accr8-project/precache-manifest.bbc8788ebe41fef7b365599f3768b25e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/accr8-project/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
